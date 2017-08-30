725d2e7626f0e4cd56f56af71b90915fc69f4096
shaojun <wusj2017@gmail.com>
2017-08-30 14:11:03 +0800

    +直接在通知栏下载，不需要询问+++

--- app/src/main/java/com/fruit/pro/update/MainActivity.java
+++ app/src/main/java/com/fruit/pro/update/MainActivity.java
@@ -52,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     });
 
 
+
+
     TextView textView = (TextView) findViewById(R.id.textView1);
 
     textView.setText("当前版本信息: versionName = " + AppUtils.getVersionName(this) + " versionCode = " + AppUtils.getVersionCode(this));
